dfd324f9d124b46e48fdbd9fd4e184e8b366da6b
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-12-09 11:18:24 +0530

    swing constructor

--- SwingConstructor/src/SwingConstructorForm.java
+++ SwingConstructor/src/SwingConstructorForm.java
@@ -1,9 +1,20 @@
+import javax.swing.*;
+
 public class SwingConstructorForm {
 
+    SwingConstructorForm(){
+        JFrame f = new JFrame();
+        JButton b = new JButton("click");
+        b.setBounds(200,300,300,150);
+        f.add(b);
+        f.setSize(700,700);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
 
 
     public static void main(String[] args) {
-        System.out.println("hello");
+        new SwingConstructorForm();
     }
 
 }
