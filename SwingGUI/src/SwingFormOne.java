2747e187f5dbf45fb73b7f07f838857d57f6db86
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-12-09 10:04:23 +0530

    First Swing GUI

---
+++ SwingGUI/src/SwingFormOne.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class SwingFormOne {
+    public static void main(String[] args){
+        JFrame f = new JFrame();
+        JButton b = new JButton("click");
+        b.setBounds(300,300,300,40);
+        f.add(b);
+        f.setSize(700,700);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+}
