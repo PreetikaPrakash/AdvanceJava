12558c7ef4c36fdda3488fa2c036f607dfe9dfe7
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-12-09 11:18:42 +0530

    swing form

--- SwingGUI/src/SwingFormOne.java
+++ SwingGUI/src/SwingFormOne.java
@@ -4,7 +4,7 @@ public class SwingFormOne {
     public static void main(String[] args){
         JFrame f = new JFrame();
         JButton b = new JButton("click");
-        b.setBounds(300,300,300,40);
+        b.setBounds(200,300,300,40);
         f.add(b);
         f.setSize(700,700);
         f.setLayout(null);
